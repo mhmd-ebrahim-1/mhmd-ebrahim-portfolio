1e55901624e613334e2e29c4f3d0ca75ccb968f7
Mohamed Ebrahim <mohammedebrahim1177@gmail.com>
2026-09-14 16:04:04 +0300

    Fix GitHub Pages asset base URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mhmd-ebrahim-portfolio/',
+  base: 'https://mhmd-ebrahim-1.github.io/mhmd-ebrahim-portfolio/',
   plugins: [react()],
 })
