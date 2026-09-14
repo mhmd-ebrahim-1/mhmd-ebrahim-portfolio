73302dddef23a93e393bfd6307bfcc061531ea81
Mohamed Ebrahim <mohammedebrahim1177@gmail.com>
2026-09-14 16:08:02 +0300

    Add resilient inline portfolio images

---
+++ src/portfolioImages.js
@@ -0,0 +1,5 @@
+export const PORTFOLIO_IMAGES = {
+  profile: 'data:image/webp;base64,UklGRmQmAABXRUJQVlA4IGgmAACwAQCdASoQAAkABUB8JaQAA3AA/vuUAAA...',
+  personal: 'data:image/webp;base64,...',
+  certificate: 'data:image/webp;base64,...',
+}
