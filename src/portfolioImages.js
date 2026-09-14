64fee5685054da140a6f591b2f68a402f8193eaa
Mohamed Ebrahim <mohammedebrahim1177@gmail.com>
2026-09-14 16:08:19 +0300

    Remove incomplete inline image module

--- src/portfolioImages.js
+++
@@ -1,5 +0,0 @@
-export const PORTFOLIO_IMAGES = {
-  profile: 'data:image/webp;base64,UklGRmQmAABXRUJQVlA4IGgmAACwAQCdASoQAAkABUB8JaQAA3AA/vuUAAA...',
-  personal: 'data:image/webp;base64,...',
-  certificate: 'data:image/webp;base64,...',
-}
